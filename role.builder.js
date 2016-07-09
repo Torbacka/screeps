d0ac5ba9d48152b69236963e8bfbed7dfcac9364
LAPTOP\Daniel Törbacka <daniel.torbacka@gmail.com>
2016-07-09 18:52:26 +0200

    refactor role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -9,33 +9,22 @@ var roleBuilder = {
 	    if(!creep.memory.transfering && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.transfering = true;
 	    }
-        log("test");
+
 	    if(creep.memory.transfering) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            } else {
-                
-                //If there is no construction sites go and repair 
-                var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_RAMPART && structure.hits < structure.hitsMax*0.1) ||
-                        (structure.structureType == STRUCTURE_WALL && structure.hits < structure.hitsMax*0.0001)||
-                        (structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax*0.8) ;
-                    }
-                });
-                if(targets.length > 0) {
-                    if(creep.repair(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
-                    } 
-                } else {
-                    //If there is nothing to do go and upgrade
-                    if( creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE ) {
-                        creep.moveTo(creep.room.controller);
-                    }
-                }
+	        
+            var task = calculateTasks(creep);
+            switch(task[0]) {
+                case 'repairWall' :
+                    repairWall(creep, task[1]);
+                    break;
+                case 'build' :
+                    build(creep, task[1]);
+                    break;
+                case 'repair' :
+                    repair(creep, task[1]);
+                    break;
+                default:
+                    upgradeController(creep);
             }
 	    }
 	    else {
@@ -45,9 +34,56 @@ var roleBuilder = {
             }
 	    }
 	}
-    function log(message) {
-        console.log(message);
-    }
+   
 };
 
+function calculateTasks(creep) {
+    //repairWAll
+    targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_RAMPART && structure.hits < structure.hitsMax*0.1) ||
+                        (structure.structureType == STRUCTURE_WALL && structure.hits < structure.hitsMax*0.0001);
+                    }
+                });
+    if(targets.length > 0) {
+        return ['repairWall', targets];
+    }
+    //Build
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if(targets.length> 0) {
+        return ['build',targets];
+    }
+    //repair rest
+    targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_ROAD && structure.hits < structure.hitsMax*0.9) ||
+                        (structure.structureType == STRUCTURE_CONTAINER && structure.hits < structure.hitsMax);
+                    }
+                });
+    if(targets.length > 0) {
+        return ['repair', targets];
+    }
+}
+function build(creep, targets) {
+        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0]);
+        }
+}
+//Send one creep to one wall
+function repairWall(creep, targets) {
+    if(creep.repair(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+    }
+}
+//Send one creep to one wall
+function repair(creep, targets) {
+    if(creep.repair(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+    }
+}
+function upgradeController(creep) {
+    if( creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE ) {
+        creep.moveTo(creep.room.controller);
+    }
+}
 module.exports = roleBuilder;
--- spawn memory.js
+++ spawn memory.js
@@ -1,14 +1,14 @@
 Game.spawns.ComandCenter.memory.creepSpecs = 
-{'harvester':[WORK,CARRY,MOVE,MOVE,MOVE], 
-'builder':[WORK,CARRY,MOVE,MOVE,MOVE],
- 'upgrader':[WORK,CARRY,MOVE,MOVE,MOVE],
+{'harvester':[WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE],
+'builder':[WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE],
+ 'upgrader':[WORK,WORK,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE],
  'defender':[ATTACK,ATTACK,ATTACK,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,MOVE,MOVE,MOVE]};
 
- Game.spawns.ComandCenter.memory.minPopulation = {'harvester':7, 'builder':6, 'upgrader':6,'defender':0};
+ Game.spawns.ComandCenter.memory.minPopulation = {'harvester':5, 'builder':3, 'upgrader':5,'defender':0};
 
  Game.spawns.ComandCenter.memory.creepSpecs['defender'] = [ATTACK,TOUGH,TOUGH,MOVE,MOVE,MOVE]
 
 
-17590
 
-16025
+
+
