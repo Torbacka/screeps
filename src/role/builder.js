c3e0f02275b238b6b1a47c2632037a9d638d7143
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-11-25 10:07:30 +0100

    Optimize builder

--- src/role/builder.js
+++ src/role/builder.js
@@ -20,7 +20,7 @@ const builder = {
         }
 
         if (creep.memory.building) {
-            const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            const targets = creep.room.findClosestByRange(FIND_CONSTRUCTION_SITES);
             if (targets.length) {
                 if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
