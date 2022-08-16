ea06c4cc1862a0eb21535f0f2829184836aa59f6
Daniel Törbacka <daniel.torbacka@gmail.com>
2022-08-16 22:07:19 +0200

    Fix merge conflict

--- src/main.js
+++ src/main.js
@@ -1,87 +1,8 @@
-<<<<<<< HEAD
-let roleHarvester = require('role_harvester');
-let roleUpgrader = require('role_upgrader');
-let roleBuilder = require('role_builder');
-let roleAttacker = require('role_attacker');
-let roleClaimer = require('role_claim');
-let roleTransporter = require('role_transporter');
-let roleMiner = require('role_miner');
-let roleMineralMiner = require('role_mineralMiner');
-let roleUpgraderHelper = require('role_upgraderHelper');
-let roleBuilderHelper = require('role_builderHelper');
-let roleDefender = require('role_defender');
-let garbagecollector = require('garbagecollector');
-let populate = require('populate');
-let market = require('market');
-let tower = require('tower');
-let factory = require('factory');
-let resourceManager = require('role_resourceManager');
-let gatherStatistics = require('gatherStatistics');
-const profiler = require('screeps-profiler');
-require('constants');
-
-profiler.enable();
-module.exports.loop = function () {
-    profiler.wrap(function () {
-        Object.values(Game.rooms).forEach((room) => {
-            room.find(FIND_MY_CREEPS).forEach((creep) => {
-                if (!_.has(creep.memory, 'role')) {
-                    return;
-                }
-                if (creep.memory.role === HARVESTER) {
-                    roleHarvester.run(creep);
-                }
-                if (creep.memory.role === UPGRADER) {
-                    roleUpgrader.run(creep);
-                }
-                if (creep.memory.role === BUILDER) {
-                    roleBuilder.run(creep, null, "W37N34");
-                }
-                if (creep.memory.role === ATTACKER) {
-                    roleAttacker.run(creep, "W38N34");
-                }
-                if (creep.memory.role === CALIMER) {
-                    roleClaimer.run(creep, "W38N34");
-                }
-                if (creep.memory.role === TRANSPORTER) {
-                    roleTransporter.run(creep);
-                }
-                if (creep.memory.role === MINER) {
-                    roleMiner.run(creep);
-                }
-                if (creep.memory.role === UPGRADER_HELPER) {
-                    roleUpgraderHelper.run(creep, null, "W39N33");
-                }
-                if (creep.memory.role === DEFENDER) {
-                    roleDefender.run(creep);
-                }
-                if (creep.memory.role === BUILDER_HELPER) {
-                    roleBuilderHelper.run(creep, null, "W38N34");
-                }if (creep.memory.role === 'mineralMiner') {
-                    roleMineralMiner.run(creep);
-                }
-                resourceManager.run(creep);
-            });
-            tower.guard(room);
-            factory.build(room);
-            populate.run(room);
-            market.trade(room);
-            gatherStatistics(room);
-            garbagecollector();
-
-
-        });
-
-    });
-};
-=======
 var roleHarvester = require('components/roles/harvester.js');
 var roleUpgrader = require('components/roles/upgrader.js');
 var roleBuilder = require('components/roles/builder.js');
-var tower = require('components/static/tower.js');
 let garbagecollector = require('lib/garbagecollector.js');
 let gatherStatistics = require('lib/gatherStatistics.js');
-let market = require('lib/market.js');
 
 module.exports.loop = function () {
     
@@ -99,7 +20,7 @@ module.exports.loop = function () {
             spawn.spawnCreep([WORK,CARRY,MOVE,MOVE], newName, 
                 {memory: {role: 'harvester'}});
         }
-        else if(harvesters.length < 2) {
+        else if(harvesters.length < 3) {
             var newName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
             spawn.spawnCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE], newName, 
@@ -139,7 +60,5 @@ module.exports.loop = function () {
         }
         gatherStatistics(room);
         garbagecollector();
-        tower.guard(room);
     });
 }
->>>>>>> 579254a (First commit on version 2)
