f529b86604a3b1c5de3d02662e9518afd05a18a4
Daniel Törbacka <daniel.torbacka@gmail.com>
2024-12-28 11:19:04 +0100

    Add profiler

--- src/main.js
+++ src/main.js
@@ -19,73 +19,77 @@ const drawing = require('./util/drawing.js');
 const tower = require('./static/tower.js');
 const safeModeActivator = require('./static/safeModeActivator.js');
 const market = require('./static/market/mainMarket.js');
+const profiler = require('./screeps-profiler.js');
 
+
+profiler.enable();
 module.exports.loop = function () {
-    if (Game.cpu.bucket === 10000) {
-        Game.cpu.generatePixel();
-    }
-    for (const roomName in Game.rooms) {
-        spawner(roomName);
-        const creepsInRoom = _.filter(Game.creeps, creep => creep.room.name === roomName);
-        creepsInRoom.forEach(creep => {
-
-            switch (creep.memory.role) {
-                case 'harvester':
-                    roleHarvester.run(creep);
-                    break;
-                case 'builder':
-                    roleBuilder.run(creep);
-                    break;
-                case 'upgrader':
-                    roleUpgrader.run(creep);
-                    break;
-                case 'Transporter':
-                    roleTransporter.run(creep);
-                    break;
-                case 'destroyer':
-                    roleDestroyer.run(creep);
-                    break;
-                case 'attacker':
-                    roleAttacker.run(creep);
-                    break;
-                case 'generalist':
-                    roleGeneralist.run(creep);
-                    break;
-                case 'mineralHarvester':
-                    roleMineralHarvester.run(creep);
-                    break;
-                case 'remoteTransporter':
-                    roleRemoteTransporter.run(creep, 'E51S33', 'E51S32');
-                    break;
-                case 'remoteHarvester':
-                    roleRemoteHarvester.run(creep, 'E58S34', 'E59S34');
-                    break;
-                case 'remoteAttacker':
-                    roleRemoteAttacker.run(creep, 'E58S34', 'E56S34');
-                    break;
-                case 'remoteClaimer':
-                    roleRemoteClaimer.run(creep, 'E58S34', 'E47S31');
-                    break;
-                case 'remoteBuilder':
-                    roleRemoteBuilder.run(creep, 'E58S34', 'E57S35');
-                    break;
-                case 'remoteHealer':
-                    roleRemoteHealer.run(creep, 'E58S34', 'E56S34');
-                    break;
-            }
-        });
-
-        let room = Game.rooms[roomName];
-        tower.guard(room);
-        if (room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TERMINAL}}).length > 0) {
-            market();
+    profiler.wrap(function () {
+        if (Game.cpu.bucket === 10000) {
+            Game.cpu.generatePixel();
         }
+        for (const roomName in Game.rooms) {
+            spawner(roomName);
+            const creepsInRoom = _.filter(Game.creeps, creep => creep.room.name === roomName);
+            creepsInRoom.forEach(creep => {
 
-    }
-    garbageCollector();
-    stats.roomStats();
-    drawing();
-    safeModeActivator();
+                switch (creep.memory.role) {
+                    case 'harvester':
+                        roleHarvester.run(creep);
+                        break;
+                    case 'builder':
+                        roleBuilder.run(creep);
+                        break;
+                    case 'upgrader':
+                        roleUpgrader.run(creep);
+                        break;
+                    case 'Transporter':
+                        roleTransporter.run(creep);
+                        break;
+                    case 'destroyer':
+                        roleDestroyer.run(creep);
+                        break;
+                    case 'attacker':
+                        roleAttacker.run(creep);
+                        break;
+                    case 'generalist':
+                        roleGeneralist.run(creep);
+                        break;
+                    case 'mineralHarvester':
+                        roleMineralHarvester.run(creep);
+                        break;
+                    case 'remoteTransporter':
+                        roleRemoteTransporter.run(creep, 'E51S33', 'E51S32');
+                        break;
+                    case 'remoteHarvester':
+                        roleRemoteHarvester.run(creep, 'E58S34', 'E59S34');
+                        break;
+                    case 'remoteAttacker':
+                        roleRemoteAttacker.run(creep, 'E58S34', 'E56S34');
+                        break;
+                    case 'remoteClaimer':
+                        roleRemoteClaimer.run(creep, 'E58S34', 'E47S31');
+                        break;
+                    case 'remoteBuilder':
+                        roleRemoteBuilder.run(creep, 'E58S34', 'E57S35');
+                        break;
+                    case 'remoteHealer':
+                        roleRemoteHealer.run(creep, 'E58S34', 'E56S34');
+                        break;
+                }
+            });
 
+            let room = Game.rooms[roomName];
+            tower.guard(room);
+            if (room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TERMINAL}}).length > 0) {
+                market();
+            }
 
+        }
+        garbageCollector();
+        stats.roomStats();
+        drawing();
+        safeModeActivator();
+
+    });
 }
